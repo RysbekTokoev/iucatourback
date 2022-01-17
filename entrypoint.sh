444d3df7eed3b40c0e1d0947e76e6a87e23d20d9
Rybsek Tokoev <rysbek@tokoev.com>
2022-01-17 18:10:01 +0600

    gunicorn -> runserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8010
+python manage.py runserver 0.0.0.0:8010
+#gunicorn config.wsgi:application --bind 0.0.0.0:8010
